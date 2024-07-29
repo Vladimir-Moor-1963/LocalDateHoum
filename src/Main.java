3ce2deae0194b9505b1aec3d432228835cfdee4e
Vladimir <papa1963@hotmail.de>
2024-07-29 15:26:22 +0200

    data

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<ProgLanguage> progLanguageList = new ArrayList<>();
-        progLanguageList.add( new ProgLanguage("Cyrillic","USSR",LocalDate.of(1978,04,15)));
+        progLanguageList.add( new ProgLanguage("Cyrillic","USSR",LocalDate.of(1978,4,15)));
         progLanguageList.add( new ProgLanguage("Hieroglyph","China",LocalDate.of(1980,5,12)));
         progLanguageList.add( new ProgLanguage("Latin","USA",LocalDate.of(1975,6,24)));
 
